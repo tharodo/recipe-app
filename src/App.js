216a51c5195fb8ed23744e9918ce37bc42d9c750
thao phuong <dophuongthao.459@gmail.com>
2021-11-22 17:35:19 +0700

    Add API request

--- src/App.js
+++ src/App.js
@@ -2,22 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const APP_ID = '8694fae1';
+  const APP_KEY = '86919c31aa9cb49e6aca72731941f836';
+
+
+  const exampleRequest = `https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello from React</h1>
     </div>
   );
 }
